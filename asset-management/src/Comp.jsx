3fee1f270910a53da7f8d77186d7227d0d44f690
Avadhut Prabhudesai <avadhutprabhudesai23@gmail.com>
2022-04-17 11:22:12 +0530

    file processing and asset management explored.

---
+++ asset-management/src/Comp.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Hi This is JSX</div>;
+}
+
+export default App;
---
+++ asset-management/webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.jsx?$/i,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'babel-loader',
+      //       options: {
+      //         presets: ['@babel/preset-env', '@babel/preset-react'],
+      //         targets: 'defaults, not IE <= 11',
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'jsx',
+              target: 'es2015',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
